9f630de1e457eb0938c8170b0ff0846d202237ab
QAQXiYangYang <1002522146@qq.com>
2017-09-08 14:42:31 +0800

    final

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import 'iview/dist/iview.min.js'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import VueResource from 'vue-resource'
+import App from './components/App'
+
+// 使用插件
+Vue.use(VueResource)
+Vue.use(iView)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+  
+})
+
+
